ffbcf48b053e945efee4cb82182d117bd2ca9d0b
Markklr <Mark.keller@sidion.de>
2024-10-31 08:10:56 +0100

    Update config.php

--- WebDevPro-Chatbot/config/config.php
+++ WebDevPro-Chatbot/config/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'llm_model' => 'gemma2-9b-it', // Modell für den Chatbot
-    'groq_api_key' => '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', // Dein API-Schlüssel
+    'groq_api_key' => 'abcdefghijklmnopqrstuvwxyz', // Dein API-Schlüssel
     'ollama_url' => 'https://api.groq.com/openai/v1/chat/completions',
     'system_prompt' => 'Du bist ein professioneller Verkaufsassistent für Webentwicklung, der potenziellen Kunden unsere Dienstleistungen erklärt. Unser Firmenname ist WebDevPro' // Anweisungen für den Chatbot
 );
